90158e343f92d234c5b52aa14e7175cdc5488e00
Jason Lin <smallhanley@gmail.com>
2020-12-13 21:10:13 +0800

    Finish 'SCAN'

--- hw3/kvs.cpp
+++ hw3/kvs.cpp
@@ -1,5 +1,6 @@
 #include <libgen.h>
 #include <unistd.h>
+#include <sys/types.h>
 #include <cerrno>
 #include <cstdio>
 #include <cstdlib>
@@ -65,7 +66,6 @@ int main(int argc, char *argv[])
     while (fscanf(fin, "%s", instr) != EOF) {
         if (!strcmp(instr, "PUT")) {
             fscanf(fin, "%llu %s", &key1, inputBuf);
-            printf("PUT %llu\n", key1);
             tag = key1 >> MEM_OFFSET_BIT;
             offset = key1 & MEM_OFFSET_CONST;
             fileName = tag >> FILE_OFFSET_BIT;
@@ -102,7 +102,6 @@ int main(int argc, char *argv[])
                             fseek(ftmp2, 0, SEEK_SET);
                             fprintf(ftmp2, "%-20u\n", pos + 1);
                         }
-                        printf("pos: %u\n", pos);
                         fseek(ftmp2, 21 + pos * BLOCK_NUM * (VAL_SIZE + 1),
                               SEEK_SET);
                         fwrite(memTable[discard].val, 1,
@@ -113,10 +112,10 @@ int main(int argc, char *argv[])
                     if (find_file_pos(tag, &pos)) {
                         fseek(ftmp1, 0, SEEK_SET);
                         fprintf(ftmp1, "%-20u\n", pos + 1);
+                        truncate(buf, 21 + (pos + 1) * BLOCK_NUM * (VAL_SIZE + 1));
                     }
-                    printf("pos: %u\n", pos);
                     fseek(ftmp1, 21 + pos * BLOCK_NUM * (VAL_SIZE + 1), SEEK_SET);
-                    fread(memTable[discard].val, 1, BLOCK_NUM * (VAL_SIZE + 1), ftmp1);
+                    fread(memTable[discard].val, (VAL_SIZE + 1), BLOCK_NUM, ftmp1);
                     fclose(ftmp1);
                     tableIndex[tag] = discard + 1;
                 }
@@ -129,7 +128,6 @@ int main(int argc, char *argv[])
         }
         else if (!strcmp(instr, "GET")) {
             fscanf(fin, "%llu", &key1);
-            printf("GET %llu\n", key1);
             tag = key1 >> MEM_OFFSET_BIT;
             offset = key1 & MEM_OFFSET_CONST;
             fileName = tag >> FILE_OFFSET_BIT;
@@ -159,7 +157,6 @@ int main(int argc, char *argv[])
                         fseek(ftmp2, 0, SEEK_SET);
                         fprintf(ftmp2, "%-20u\n", pos + 1);
                     }
-                    printf("pos: %u\n", pos);
                     fseek(ftmp2, 21 + pos * BLOCK_NUM * (VAL_SIZE + 1), SEEK_SET);
                     fwrite(memTable[discard].val, 1, BLOCK_NUM * (VAL_SIZE + 1), ftmp2);
                     fclose(ftmp2);
@@ -168,12 +165,8 @@ int main(int argc, char *argv[])
                 if (find_file_pos(tag, &pos)) {
                     fseek(ftmp1, 0, SEEK_SET);
                     fprintf(ftmp1, "%-20u\n", pos + 1);
+                    truncate(buf, 21 + (pos + 1) * BLOCK_NUM * (VAL_SIZE + 1));
                 }
-                unsigned int tmp;
-                // hash_table_get(0, &tmp);
-                // printf("tmp: %u\n", tmp);
-                // printf("pos: %u\n", pos);
-                printf("pos: %u\n", pos);
                 fseek(ftmp1, 21 + pos * BLOCK_NUM * (VAL_SIZE + 1), SEEK_SET);
                 fread(memTable[discard].val, 1, BLOCK_NUM * (VAL_SIZE + 1), ftmp1);
                 fclose(ftmp1);
@@ -192,20 +185,62 @@ int main(int argc, char *argv[])
         }
         else if (!strcmp(instr, "SCAN")) {
             fscanf(fin, "%llu %llu", &key1, &key2);
-            // for (unsigned long long key = key1; key <= key2; key++) {
-            //     tag = key >> MEM_OFFSET_BIT;
-            //     offset = key & MEM_OFFSET_CONST;
-            //     if (!tableIndex[tag]) {
-            //     }
-            //     if (memTable[tableIndex[tag] - 1].val[offset][0] == '1') {
-            //         fwrite(memTable[tableIndex[tag] - 1].val[offset] + 1, 1,
-            //                VAL_SIZE, fout);
-            //     }
-            //     else {
-            //         fprintf(fout, "EMPTY");
-            //     }
-            //     fprintf(fout, "\n");
-            // }
+            for (unsigned long long key = key1; key <= key2; key++) {
+                tag = key >> MEM_OFFSET_BIT;
+                offset = key & MEM_OFFSET_CONST;
+                fileName = tag >> FILE_OFFSET_BIT;
+                sprintf(buf, "storage/%u.tmp", fileName);
+                if (access(buf, 0) < 0) {
+                    ftmp1 = fopen(buf, "w+");
+                    fprintf(ftmp1, "0                   \n");
+                    fclose(ftmp1);
+                }
+                if (!tableIndex[tag]) {
+                    if (!(ftmp1 = fopen(buf, "r+"))) {
+                        fprintf(stderr, "ERROR: unable to open '%s': %s\n\n",
+                                buf, strerror(errno));
+                        exit(1);
+                    }
+                    int discard = rand() % TABLE_SIZE;
+                    tableIndex.erase(memTable[discard].tag);
+                    if (memTable[discard].dirty) {
+                        unsigned int fileName2 = memTable[discard].tag >> FILE_OFFSET_BIT;
+                        sprintf(buf2, "storage/%u.tmp", fileName2);
+                        if (!(ftmp2 = fopen(buf2, "r+"))) {
+                            fprintf(stderr, "ERROR: unable to open '%s': %s\n\n", buf2, strerror(errno));
+                            exit(1);
+                        }
+                        fscanf(ftmp2, "%u", &pos);
+                        if (find_file_pos(memTable[discard].tag, &pos)) {
+                            fseek(ftmp2, 0, SEEK_SET);
+                            fprintf(ftmp2, "%-20u\n", pos + 1);
+                        }
+                        fseek(ftmp2, 21 + pos * BLOCK_NUM * (VAL_SIZE + 1), SEEK_SET);
+                        fwrite(memTable[discard].val, 1, BLOCK_NUM * (VAL_SIZE + 1), ftmp2);
+                        fclose(ftmp2);
+                    }
+                    fscanf(ftmp1, "%u", &pos);
+                    if (find_file_pos(tag, &pos)) {
+                        fseek(ftmp1, 0, SEEK_SET);
+                        fprintf(ftmp1, "%-20u\n", pos + 1);
+                        truncate(buf, 21 + (pos + 1) * BLOCK_NUM * (VAL_SIZE + 1));
+                    }
+                    fseek(ftmp1, 21 + pos * BLOCK_NUM * (VAL_SIZE + 1), SEEK_SET);
+                    fread(memTable[discard].val, 1, BLOCK_NUM * (VAL_SIZE + 1), ftmp1);
+                    fclose(ftmp1);
+                    tableIndex[tag] = discard + 1;
+                    memTable[discard].dirty = false;
+                }
+                memTable[tableIndex[tag] - 1].tag = tag;
+                if (memTable[tableIndex[tag] - 1].val[offset][0] == '1') {
+                    fwrite(memTable[tableIndex[tag] - 1].val[offset] + 1, 1,
+                        VAL_SIZE, fout);
+                }
+                else {
+                    fprintf(fout, "EMPTY");
+                }
+                fprintf(fout, "\n");
+            }
         }
         else {
             fprintf(stderr, "ERROR: instruction not found\n\n");
@@ -215,7 +250,6 @@ int main(int argc, char *argv[])
 
     for (int i = 0; i < TABLE_SIZE; i++) {
         if (memTable[i].dirty) {
-            printf("%d %llu\n", i, memTable[i].tag);
             unsigned int fileName2 = memTable[i].tag >> FILE_OFFSET_BIT;
             unsigned int pos;
             sprintf(buf2, "storage/%d.tmp", fileName2);
